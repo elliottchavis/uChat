1660c398a6a56f998c2f04ce759322c6ac925b2a
agileassociates <agileassociates@agileastessMini.lan>
2021-05-02 21:39:19 -0400

    added profile picture

--- uChat/Controller/ProfileController.swift
+++ uChat/Controller/ProfileController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 private let reuseIdentifier = "ProfileCell"
 
@@ -14,18 +15,16 @@ class ProfileController: UITableViewController {
     // MARK: - Properties
     
     private lazy var headerView = ProfileHeader(frame: .init(x: 0, y: 0, width: view.frame.width, height: 380))
-    
+    private var user: User? {
+        didSet { headerView.user = user }
+    }
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let gradient = CAGradientLayer()
-//        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemPink.cgColor]
-//        gradient.locations = [0, 1]
-//        headerView.layer.addSublayer(gradient)
-        //gradient.frame = bounds
         headerView.backgroundColor = .systemBlue
         configureUI()
+        fetchUser()
         
 
     }
@@ -39,11 +38,20 @@ class ProfileController: UITableViewController {
     
     // MARK: - API
     
+    func fetchUser() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Service.fetchUser(withUid: uid) { user in
+            self.user = user
+            print("\n\n\n\n\n\n\n\n\n\n\n\nDEBUG: User is \(user.username)")
+        }
+    }
+    
     // MARK: - Helpers
     
     func configureUI() {
         tableView.backgroundColor = .white
         tableView.tableHeaderView = headerView
+        headerView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.tableFooterView = UIView()
     }
@@ -65,4 +73,10 @@ extension ProfileController {
 //    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
 //        let headerView = self.configureHeaderGradient()
 //    }
+}
+
+extension ProfileController: ProfileHeaderDelegate {
+    func dismissController() {
+        dismiss(animated: true, completion: nil)
+    }
 }
