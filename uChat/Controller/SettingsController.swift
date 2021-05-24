b58718c9b269c2ea89c3b6715bee8894f11dab12
johny babylon <ztredded@gmail.com>
2021-05-24 01:45:11 -0400

    fixed themes, navBar & picker

--- uChat/Controller/SettingsController.swift
+++ uChat/Controller/SettingsController.swift
@@ -1,137 +1,58 @@
 //
-//  SettingsController.swift
+//  NewController.swift
 //  uChat
 //
-//  Created by agileassociates on 5/18/21.
+//  Created by elliott chavis on 5/23/21.
 //
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
-protocol ColorDelegate: AnyObject {
-    func changeColor(_ color: UIColor?)
-}
-
-class SettingsController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class SettingsController: UIViewController {
     
-    // MARK: - Properties
-    
-   // weak var delegate: ColorDelegate?
+    private let themeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .systemBlue
+        button.titleLabel?.text = "Themes"
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(showThemes), for: .touchUpInside)
 
-    private let dismissButton: UIButton = {
-            let button = UIButton(type: .system)
-            button.setImage(UIImage(systemName: "xmark"), for: .normal)
-            button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
-            button.backgroundColor = .systemBlue
-            button.tintColor = .white
-            button.imageView?.setDimensions(height: 22, width: 22)
-            return button
-        }()
-    
-    private let themeLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Themes"
-        return label
+        return button
     }()
     
-    // MARK: - Lifecycle
-    
-    override func viewDidLoad() {
+    //MARK: - #Lifecycles
+    override func viewDidLoad(){
         super.viewDidLoad()
-        configureUI()
-        
+        configurUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         configureNavigationBar(withTitle: "Settings", prefersLargeTitles: true)
         let image = UIImage(systemName: "xmark")
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(exit))
-
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(handleDismissal))
     }
     
-    // MARK: - Selectors
+    //MARK: - #Selectors
+    
+    @objc func showThemes() {
+        let controller = ThemeController()
+        let nav = UINavigationController(rootViewController: controller)
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: true, completion: nil)
+
+    }
     
     @objc func handleDismissal() {
         self.dismiss(animated: true, completion: nil)
     }
     
-    @objc func exit() {
-        let controller = ProfileController()
-        let nav = UINavigationController(rootViewController: controller)
-
-        show(controller, sender: self)
-    }
+    //MARK: - #Helpers
     
-    // MARK: - Helpers
-    
-    func configureUI() {
+    func configurUI() {
+        
         view.backgroundColor = .white
-        
-        
-        view.addSubview(dismissButton)
-                dismissButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 3)
-                
-                view.addSubview(themeLabel)
-                themeLabel.setDimensions(height: 15, width: 64)
-                themeLabel.anchor(top: dismissButton.bottomAnchor, paddingTop: 10)
-                
-        
-        
-        
-//        view.addSubview(themeLabel)
-//        themeLabel.setDimensions(height: 15, width: 64)
-//        themeLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 10)
-        
-        
-        let picker = UIPickerView()
-        picker.delegate = self
-        picker.dataSource = self
-        picker.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(picker)
-        
-        picker.anchor(top: themeLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: -20)
 
-//        picker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-//        picker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-//        picker.topAnchor.constraint(equalTo: themeLabel.bottomAnchor).isActive = true
-        
-        
-    }
-    
-    // MARK: - PickerDataSource
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 2
-    }
-
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        let pickerData = ["Default", "Miami"]
-        return pickerData[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if row == 0 {
-            ThemeManager.sharedInstance.setTheme(userTheme: "Default")
-            print(ThemeManager.sharedInstance.getTheme())
-            
-            
-            
-
-            
-        } else {
-            ThemeManager.sharedInstance.setTheme(userTheme: "Miami")
-            print(ThemeManager.sharedInstance.getTheme())
-            
-            
-            
-
-        }
-        
+        view.addSubview(themeButton)
+        themeButton.setDimensions(height: 40, width: view.frame.width)
+        themeButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor)
     }
 }
---
+++ uChat/Controller/ThemeController.swift
@@ -0,0 +1,92 @@
+//
+//  ThemeController.swift
+//  uChat
+//
+//  Created by elliott chavis on 5/23/21.
+//
+
+import UIKit
+import JGProgressHUD
+
+class ThemeController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    //MARK: - #Lifecycles
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            configureUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureNavigationBar(withTitle: "Themes", prefersLargeTitles: true)
+        let image = UIImage(systemName: "xmark")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(handleDismissal))
+    }
+
+    
+    // MARK: - Selectors
+    
+    @objc func handleDismissal() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: - #Helpers
+    
+    func configureUI() {
+        
+        view.backgroundColor = .white
+        
+        
+        let picker = UIPickerView()
+        picker.delegate = self
+        picker.dataSource = self
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        
+        if ThemeManager.sharedInstance.getTheme() == "Default"{
+            picker.selectRow(0, inComponent: 0, animated: true)
+        } else {
+            picker.selectRow(1, inComponent: 0, animated: true)
+        }
+        
+        view.addSubview(picker)
+        
+        picker.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: -20)
+    }
+    
+    
+    //MARK: - #PickerDataSource
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 2
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let pickerData = ["Default", "Miami"]
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if row == 0 {
+            ThemeManager.sharedInstance.setTheme(userTheme: "Default")
+            print(ThemeManager.sharedInstance.getTheme())
+        } else {
+            ThemeManager.sharedInstance.setTheme(userTheme: "Miami")
+            print(ThemeManager.sharedInstance.getTheme())
+        }
+        let hud = JGProgressHUD()
+        hud.textLabel.text = "Loading"
+        hud.show(in: self.view)
+        hud.dismiss(afterDelay: 0.5)
+        
+        let secondsToDelay = 0.7
+        DispatchQueue.main.asyncAfter(deadline: .now() + secondsToDelay) {
+           print("This message is delayed")
+            self.view.window!.rootViewController?.dismiss(animated: false, completion: nil)
+        }
+    }
+    
+}
